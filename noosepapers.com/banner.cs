d31f5ef565f3db413111af3800eeb1818cef04ea
John Comeau <jc@unternet.net>
2024-01-31 16:16:44 -0800

    fixed font name, still no luck

--- noosepapers.com/banner.cs
+++ noosepapers.com/banner.cs
@@ -1,5 +1,5 @@
 #!/usr/local/casperscript/bin/cs -S -sFONTMAP=noosefonts.ps -sFONTPATH=.
-/Cloister-Black 47 selectfont
+/CloisterBlack 47 selectfont
 /inch {72 mul} def
 2 inch 2 inch moveto
 (The Noose Papers) show
