2431f5220191ee1fce6463953253a0f5bc838b22
John Comeau <jc@unternet.net>
2024-03-20 17:36:56 -0700

    works as ./banner.cs now too

--- noosepapers.com/banner.cs
+++ noosepapers.com/banner.cs
@@ -1,4 +1,4 @@
-#!/usr/local/casperscript/bin/gs -S -sFONTMAP=noosefonts.ps -sFONTPATH=. -C
+#!/usr/local/casperscript/bin/gs -S -I. -sFONTMAP=noosefonts.ps -sFONTPATH=. -C
 /CloisterBlack 47 selectfont
 /inch {72 mul} def
 2 inch 2 inch moveto
