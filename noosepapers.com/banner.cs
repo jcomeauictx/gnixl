efa726abb0db81ac339fcdd5d1665d4e683dc911
John Comeau <jc@unternet.net>
2024-03-20 20:38:57 -0700

    first attempt with graphic

--- noosepapers.com/banner.cs
+++ noosepapers.com/banner.cs
@@ -1,6 +1,11 @@
 #!/usr/local/casperscript/bin/gs -S -I. -sFONTMAP=noosefonts.ps -sFONTPATH=. -C
+(pnmimage.cs) run
 /CloisterBlack 47 selectfont
 /inch {72 mul} def
 2 inch 2 inch moveto
 (The Noose Papers) show
+2 inch 4 inch moveto
+(The Noose ) show
+gsave 0.1 0.1 scale (gallows.pgm) pnmimage grestore
+( Papers) show
 showpage
