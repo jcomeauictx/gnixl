d4b4dd9d98716157cda56f83bb8bdc3351e45a87
John Comeau <jc@unternet.net>
2024-10-18 14:47:20 -0700

    debugging statements now working, centering still all wrong

--- noosepapers.com/banner.cs
+++ noosepapers.com/banner.cs
@@ -1,6 +1,9 @@
 #!/usr/local/casperscript/bin/gs -S -I. -sFONTMAP=noosefonts.ps -sFONTPATH=. -C
 % NY POST banner font is about 1/12 page height
 (pnmimage.cs) run
+/printstack {
+  (current stack: ) print count array astore dup == aload pop
+} bind def
 /centershow { % bool bool -
   % show centered on page, vertically and/or horizontally
   /centerhorizontally exch def /centervertically exch def
@@ -16,13 +19,14 @@
   }{currentpoint pop} ifelse
   centervertically {
     (centering vertically;) print
-    heightcenter pathheight 2 div add
+    heightcenter pathheight 2 div sub
   }{currentpoint exch pop} ifelse
-  (translating to: ) print pstack
+  (translating to: ) print 2 copy 2 array astore ==
+  printstack
   translate
   show
   grestore
-  (stack at end of centershow: ) print pstack
+  printstack
 } def
 /CloisterBlack 47 selectfont
 /inch {72 mul} def
