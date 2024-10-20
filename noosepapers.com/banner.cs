d60d13bfe31596b84434d25402d5c0200bb5f8fa
John Comeau <jc@unternet.net>
2024-10-20 13:18:47 -0700

    restructed, but now final: shows empty pathbbox

--- noosepapers.com/banner.cs
+++ noosepapers.com/banner.cs
@@ -1,4 +1,4 @@
-#!/usr/local/casperscript/bin/gs -S -I. -sFONTMAP=noosefonts.ps -sFONTPATH=. -C
+#!/usr/local/casperscript/bin/gs -S -I. -sFONTMAP=noosefonts.ps -sFONTPATH=. -C --
 % NY POST banner font is about 1/12 page height
 (pnmimage.cs) run
 /centershow { % bool bool -
@@ -31,32 +31,45 @@
   grestore
   (stack: ) print =stack
 } def
-/CloisterBlack 47 selectfont
-/inch {72 mul} def
-(page height: ) print pageheight =
-(page width: ) print pagewidth =
-1 inch 1 inch moveto (The Noose Papers) false false centershow
-1 inch 2 inch moveto (The Noose Papers 2) true false centershow
-1 inch 3 inch moveto (The Noose Papers 3) false true centershow
-1 inch 4 inch moveto (The Noose Papers 4) true true centershow
-2 inch 10 inch moveto
-save
-/image {
-  dup /Decode get /decoder exch def
-  % translate all values to 1 (white)
-  0 1 decoder length 1 sub {decoder exch 1 put} for
-  (/Decoder: ) print dup /Decode get ===
-  image
+
+/banner {
+  save
+  % first, a dry run to get the banner size
+  % redefine `image` and `show` for that purpose
+  /image {
+    dup /Decode get /decoder exch def
+    % translate all values to 1 (white)
+    0 1 decoder length 1 sub {decoder exch 1 put} for
+    (/Decoder: ) print dup /Decode get ===
+    image
+  } bind def
+  /show {true charpath} def  % just append to path rather than show on page
+  1 inch 7 inch moveto
+  (first: ) print [pathbbox] == (The Noose ) show
+  (stack after first: ) print =stack
+  (second: ) print [pathbbox] == (gallows.pgm) (TNPp) pnminline
+  (third: ) print [pathbbox] == ( Papers) show
+  (final: ) print [pathbbox] ==
+  pathbbox exch 4 -1 roll sub 3 1 roll exch sub pageheight
+  (stack for determining banner position: ) print  =stack
+  restore
+  1 inch 7 inch moveto (The Noose ) show (gallows.pgm) (TNPp) pnminline ( Papers) show
+  (stack: ) print =stack
 } bind def
-/show {true charpath} def  % just append to path rather than show on page
-1 inch 7 inch moveto
-(first: ) print [pathbbox] == (The Noose ) show
-(stack after first: ) print =stack
-(second: ) print [pathbbox] == (gallows.pgm) (TNPp) pnminline
-(third: ) print [pathbbox] == ( Papers) show
-(final: ) print [pathbbox] ==
-% pathbbox exch 4 -1 roll sub 3 1 roll exch sub pageheight =stack
-restore
-1 inch 7 inch moveto (The Noose ) show (gallows.pgm) (TNPp) pnminline ( Papers) show
-(stack: ) print =stack
-showpage
+
+% test run using `cs -- banner`
+scriptname (banner) eq {
+  /CloisterBlack 47 selectfont
+  /inch {72 mul} def
+  (page height: ) print pageheight =
+  (page width: ) print pagewidth =
+  1 inch 1 inch moveto (The Noose Papers) false false centershow
+  gsave .5 setgray  % 2 vertical centershows will overlap
+  1 inch 2 inch moveto (The Noose Papers 2) true false centershow
+  grestore
+  1 inch 3 inch moveto (The Noose Papers 3) false true centershow
+  1 inch 4 inch moveto (The Noose Papers 4) true true centershow
+  banner
+  showpage
+} if
+% vim: tabstop=8 shiftwidth=2 expandtab softtabstop=2
