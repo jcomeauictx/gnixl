daec6cfa1a9913c705666f4e9a75c5bdab466fcf
John Comeau <jc@unternet.net>
2024-10-21 07:05:47 -0700

    a start!

--- noosepapers.com/columns.cs
+++ noosepapers.com/columns.cs
@@ -1,4 +1,9 @@
 #!/usr/local/casperscript/bin/cs --
 (lorem_ipsum.ps) run
-sys.argv 1 get =
-loremipsum =
+sys.argv dup length 1 gt
+  {1 get (r) file}
+  {loremipsum << /EODCount 0 /EODString () >> /SubFileDecode filter}
+  ifelse /datasource exch def
+/buffer 128 string def
+datasource buffer readstring
+ {=}{= exit} ifelse
