a8d2011157bab351a3ddd8faed3593327260b6f7
John Comeau <jc@unternet.net>
2024-10-22 09:18:38 -0700

    shows "stack at end:" on *every word*

--- noosepapers.com/columns.cs
+++ noosepapers.com/columns.cs
@@ -8,13 +8,34 @@ sys.argv dup length 1 gt
   <</EODCount 0 /EODString ( )>>
   /SubFileDecode
 } bind def
+/column {  % source -
+  /source exch def
+  /width pagewidth 5 div def
+  /height pageheight 2 div def
+  /line () def
+  {source wordparse filter
+    buffer readstring {
+      line exch stradd
+      dup stringwidth width gt
+        {gsave line show /line () def grestore 0 -10 rmoveto}
+        {( ) stradd /line exch def}
+        ifelse
+    }{
+      (stack at end of `columns`: ) print =stack exit
+    } ifelse
+  } loop
+} bind def
 scriptname (columns) eq {
   /buffer 128 string def
   {datasource wordparse filter
     buffer readstring
     {print}
-    {dup length 0 gt {print} {exit} ifelse}
+    {(stack at end: ) print =stack dup length 0 gt {print} {pop exit} ifelse}
     ifelse
   } loop
+  datasource resetfile
+  /Helvetica 12 selectfont
+  0 pageheight 10 sub moveto
+  datasource column
 } if
 % vim: tabstop=8 shiftwidth=2 expandtab softtabstop=2
