03fab9cacc5e4d0591bfa91ebe9b36506cd7f12e
John Comeau <jc@unternet.net>
2024-10-22 15:08:34 -0700

    create it *once* not every time LoremIpsum is invoked

--- noosepapers.com/lorem_ipsum.cs
+++ noosepapers.com/lorem_ipsum.cs
@@ -2,11 +2,11 @@
 /loremipsum  % the basic string
 (Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.) def
 
-/LoremIpsum {  % file-like object that produces infinite copies of the string
+/LoremIpsum  % file-like object that produces infinite copies of the string
   {loremipsum (\r\n\r\n) stradd}
   <</EODCount 0 /EODString()>>
   /SubFileDecode filter
-} bind def
+  def
 
 scriptname (lorem_ipsum) eq {
   LoremIpsum {dup 256 string readstring {print} if} loop
