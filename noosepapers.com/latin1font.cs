bbaf8abab222e0a97094d9e5a75fd5435e5d907c
John Comeau <jc@unternet.net>
2024-11-10 11:00:10 -0800

    maybe fixed, now remove verbose debugging

--- noosepapers.com/latin1font.cs
+++ noosepapers.com/latin1font.cs
@@ -2,6 +2,7 @@
 % font modified to use Latin-1 encoding
 % PLRM page 349
 /latin1font { %  fontname -
+  dup
   % add Latin1 character set to font
   findfont
   dup length dict begin {
@@ -11,11 +12,11 @@
     ifelse
   } forall
   /Encoding ISOLatin1Encoding def
-  currentdict
-  end
-  sys.argv 1 get (-Latin1) add cvn
-  dup (Made ) #only #only ( from ) #only sys.argv 1 get #
-  exch definefont pop
+  currentdict end
+  (after currentdict end: ) #only #stack
+  1 index 128 string cvs (-Latin1) add cvn #stack dup (new fontname: ) #only ##
+  dup (made ) #only #only #stack ( from ) #only 3 -1 roll #
+  exch definefont
 } bind def
 scriptname (latin1font) eq {
   sys.argv 1 get cvn latin1font
