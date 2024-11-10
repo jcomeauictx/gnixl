8744317b8a75243f48d3e7b2e8e7b4a5a8b029d7
John Comeau <jc@unternet.net>
2024-11-09 16:11:42 -0800

    rewriting columns.cs

--- noosepapers.com/columns.cs
+++ noosepapers.com/columns.cs
@@ -2,33 +2,18 @@
 (lorem_ipsum.cs) run
 (latin1font.ps) run
 (paragraphs.cs) run
-sys.argv dup length 1 gt
-  {1 get (r) file}
-  {pop loremipsum}
-  ifelse /datasource exch def
-0 1 currentfont /Encoding get 3 1 roll
-  2 index length 1 sub dup /charmap exch dict def
-  {1 index 1 index get charmap 3 1 roll exch put} for pop
-/wordbuffer 128 string def
-/wordparse {
-  <</EODCount 0 /EODString ( )>>
-  /SubFileDecode
-} bind def
 /xwidth {stringwidth pop} bind def
-/column {  % source -
-  /source exch def
-  /width pagewidth 2 div def
-  /height pageheight 2 div def
+% broadsheet typically has 5 columns, tabloid 4, zine maybe 2 or 3
+/columnwidth pagewidth 2 div def
+/columnheight pageheight 2 div def
+/columnline {  % words index - words newindex
+  /index exch def
   (column width: ) print width =only (, column height: ) print height =
   /spacewidth ( ) xwidth def
   /line () def
   (width: ) print width =
   (spacewidth: ) print spacewidth =
-  {source paragraphs filter wordparse filter
-    % readstring through wordparse filter will almost always return false
-    % (only true with 128-character word)
-    wordbuffer readstring
-      {wordbuffer /rangecheck signalerror}  % word too long
+  {source paragraphs filter
       {
         dup /word exch def
 	xwidth line xwidth spacewidth add add dup
@@ -53,19 +38,33 @@ sys.argv dup length 1 gt
   loop
 } bind def
 scriptname (columns) eq {
-  {datasource paragraphs filter wordparse filter
-    wordbuffer readstring
-    {print}  % this would only happen with 128-character "word"
-    {dup length 0 gt {print} {pop exit} ifelse}
-    ifelse
-  } loop
-  % if the above loop ended, we must be using a regular file, not LoremIpsum
-  % `resetfile` doesn't necessarily, work, so just reopen the file 
   /Helvetica-Latin1 12 selectfont
-  0 pageheight 10 sub moveto
+  (starting columns test program) #
+  sys.argv dup length 1 gt
+    {1 get (r) file}
+    {pop loremipsum}
+    ifelse /datasource exch def
+  0 1 currentfont /Encoding get 3 1 roll
+    2 index length 1 sub dup /charmap exch dict def
+    {1 index 1 index get charmap 3 1 roll exch put} for pop
+  /count zero
+  sys.argv dup length 1 gt
+    {1 get (r) file}
+    {pop LoremIpsum}
+    ifelse /datasource exch def
+  (testing column creation) #
+  {datasource paragraphs filter
+    1024 dup mul string
+    readline not /eof exch def
+    4096 array () 3 -1 roll exch string.split array.truncate ==
+    eof {(exiting on EOF) # exit} if  % quit after all data processed
+    /count inc count 10000 eq {exit} if  % quit test after 10000 paragraphs
+  } loop
+  (stack at end of columns test: ) #only ##stack
+  (bytes available: ) # datasource bytesavailable #
+  10 pageheight 10 sub moveto
   (now showing column on page) =
-  sys.argv 1 get (r) file column
   showpage
-  % charmap ===
+  (final stack: ) #only ##stack
 } if
 % vim: tabstop=8 shiftwidth=2 expandtab softtabstop=2 syntax=postscr
