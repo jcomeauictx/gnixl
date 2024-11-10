5877fdf5e04ae67504b31ef289e49654c0240b1c
John Comeau <jc@unternet.net>
2024-11-10 06:40:24 -0800

    now should work for any font

--- noosepapers.com/latin1font.cs
+++ noosepapers.com/latin1font.cs
@@ -1,14 +1,24 @@
+#!/usr/local/casperscript/bin/bccs
 % font modified to use Latin-1 encoding
 % PLRM page 349
-/Helvetica findfont
-dup length dict begin {
-  1 index /FID ne  % skip font identifier
-  {def}  % otherwise copy key-value pair
-  {pop pop}  % drop FID
-  ifelse
-} forall
-/Encoding ISOLatin1Encoding def
-currentdict
-end
-/Helvetica-Latin1 exch definefont pop
+/addlatin1 { %  fontname - fontname
+  % add Latin1 character set to font
+  sys.argv 1 get cvn findfont
+  dup length dict begin {
+    1 index /FID ne  % skip font identifier
+    {def}  % otherwise copy key-value pair
+    {pop pop}  % drop FID
+    ifelse
+  } forall
+  /Encoding ISOLatin1Encoding def
+  currentdict
+  end
+  sys.argv 1 get (-Latin1) add cvn
+  dup (Made ) #only #only ( from ) #only sys.argv 1 get #
+  exch definefont pop
+} bind def
+scriptname (latin1font) eq {
+  addlatin1
+  (stack at end: ) #only #stack
+} if
 % vim: tabstop=8 shiftwidth=2 expandtab softtabstop=2 syntax=postscr
