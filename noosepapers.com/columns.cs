de080031733396b0e98de80eeb63a5933404b893
John Comeau <jc@unternet.net>
2024-11-13 07:44:12 -0800

    preparing for `columns` procedure

--- noosepapers.com/columns.cs
+++ noosepapers.com/columns.cs
@@ -18,9 +18,10 @@
 /spacewidth ( ) xwidth def
 (spacewidth: ) #only spacewidth #
 % broadsheet typically has 5 columns, tabloid 4, zine maybe 2 or 3
-/columnwidth pagewidth margin dup add sub 3 div def
+/columnsperpage 3 def
+/columnwidth pagewidth margin dup add sub columnsperpage div def
 /linewidth columnwidth margin sub def
-/columnheight pageheight margin dup add 2 div def
+/columnheight pageheight margin dup add def
 (column width: ) #only columnwidth #only (, height: ) #only columnheight #
 /columnline {  % words index - endofparagraph newindex string
   (starting columnline with stack: ) #only #stack
