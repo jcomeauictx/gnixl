ecae99de249ef54635140ea838731694e87e7a88
John Comeau <jc@unternet.net>
2024-11-19 17:08:30 -0800

    added debugging statements

--- noosepapers.com/noosepaper.cs
+++ noosepapers.com/noosepaper.cs
@@ -1,9 +1,15 @@
 #!/usr/local/casperscript/bin/cs -S -I. -sFONTMAP=noosefonts.ps -sFONTPATH=. --
+(loading noosepaper imports, banner...) #
 /banner where {pop} {(banner.cs) run} ifelse
+(loading noosepaper imports, columns...) #
 /columns where {pop} {(columns.cs) run} ifelse
+(loading noosepaper imports, loremipsum...) #
 /loremipsum where {pop} {(loremipsum.cs) run} ifelse
+(end loading noosepaper) #
 scriptname (noosepaper) eq {
+  (starting noosepaper: ) #only #stack
   (The Noose gallows.pgm Papers) /CloisterBlack 47 banner
+  (now painting columns) #
   2.5 0 LoremIpsum columns
   showpage
 }
