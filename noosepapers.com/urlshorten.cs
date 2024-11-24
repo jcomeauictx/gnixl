6af7614221fe6f8abc25f1a787a91912e3e770e5
John Comeau <jc@unternet.net>
2024-11-24 07:55:43 -0800

    segfault

--- noosepapers.com/urlshorten.cs
+++ noosepapers.com/urlshorten.cs
@@ -2,10 +2,18 @@
 /urlshorten  ( url - shorturl
   generate a short URL for a long one) docstring {
   rand 36 10 string cvrs string.lower dup
-  (../gnixl/gnixl.com/l/) 1 index string.add dup 8#755 os.mkdir
-  (/.htaccess) string.add (w) file
-  dup (Redirect 301 . ) 4 index string.add (\n) string.add writestring
-  closefile
+  3
+    {
+      {(../gnixl.com/l/) 1 index string.add dup 8#755 os.mkdir} stopped
+        {(trying again, ) #only dup #only ( didn't work) # pop}
+        {exit}
+        ifelse
+    }
+    repeat
+  (../gnixl.com/.htaccess) string.add (a) file
+  #stack
+  %dup (Redirect 301 ) 4 index string.add (\n) string.add writestring
+  %closefile
 } bind def
 
 scriptname (urlshorten) eq
