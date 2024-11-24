2a587e3c0e53d4c9f1905d9d1bf906ec942cc7b4
John Comeau <jc@unternet.net>
2024-11-24 11:00:17 -0800

    segfaults whether or not mkdir succeeds

--- noosepapers.com/urlshorten.cs
+++ noosepapers.com/urlshorten.cs
@@ -1,15 +1,17 @@
-#!/usr/local/casperscript/bin/bccs --
+#! ../../casperscript/bin/bccs --
 /urlshorten  ( url - shorturl
   generate a short URL for a long one) docstring {
   rand 36 10 string cvrs string.lower dup
+  (stack after creating directory name: ) #only #stack
   3
     {
-      {(../gnixl.com/l/) 1 index string.add dup 8#755 os.mkdir} stopped
-        {(trying again, ) #only dup #only ( didn't work) # pop}
-        {exit}
+      {(../gnixl.com/l/) 1 index string.add dup 8#755 #stack .mkdir #stack} stopped
+        {(trying again, stack: ) #only #stack pop}
+        {(succeeded, continuing) # exit}
         ifelse
     }
     repeat
+  (after mkdir: ) #only #stack
   (../gnixl.com/.htaccess) string.add (a) file
   #stack
   %dup (Redirect 301 ) 4 index string.add (\n) string.add writestring
