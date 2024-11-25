4ac802d59db62f568a74f0d6641c659835c15fba
John Comeau <jc@unternet.net>
2024-11-25 12:26:59 -0800

    working, but leaving debugging statements in until testing in larger context

--- noosepapers.com/urlshorten.cs
+++ noosepapers.com/urlshorten.cs
@@ -1,12 +1,12 @@
 #! ../../casperscript/bin/bccs --
 /MAX_TRIES 10 def
-/urlshorten  ( url - shorturl
+/urlshorten  ( url - shorturl true OR url false
   generate a short URL for a long one) docstring {
   mark  % make it easy to clean stack regardless of where it failed
   MAX_TRIES  % sufficient number of tries to find a unique random name
     {
       {
-        rand 36 10 string cvrs string.lower
+        urand 36 10 string cvrs string.lower
         (stack after creating directory name: ) #only #stack
         (../gnixl.com/l/) exch string.add dup 8#755
         (stack before os.mkdir: ) #only #stack
@@ -21,18 +21,20 @@
     repeat
   (after mkdir: ) #only #stack
   true ne  % failure would leave only a -mark- on stack
-    {(failed after ) #only MAX_TRIES #only ( attempts) #}
+    {(failed after ) #only MAX_TRIES #only ( attempts) # false}
     {
       dup (/../../.htaccess) string.add (a) file
       exch [12] substring  % chop first part of path to form URL
       (after substring: ) #only #stack
       (after forming URL: ) #only #stack
-      (Redirect 301 ) exch string.add  % "from" URL added
+      (Redirect 301 ) 1 index string.add  % "from" URL added
       (first part of redirect: ) #only #stack
-      ( ) string.add 3 index string.add #stack
-      (\n) string.add 1 index exch writestring #stack
-      closefile #stack
-      pop  % discard `mark`
+      ( ) string.add 5 -1 roll string.add #stack
+      (\n) string.add 2 index exch writestring #stack
+      exch closefile #stack
+      exch pop  % discard `mark`
+      (gnixl.com) exch string.add
+      true
     } ifelse
     (stack after urlshorten: ) #only #stack
 } bind def
