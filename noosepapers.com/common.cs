e4fb90c2ce671b96e51347459879f9d560373e69
John Comeau <jc@unternet.net>
2024-11-27 05:06:44 -0800

    added more to common

--- noosepapers.com/common.cs
+++ noosepapers.com/common.cs
@@ -1,4 +1,5 @@
 #!/usr/local/casperscript/bin/cs -S -I. --
+/inch {72 mul} def
 /loremipsum where {pop} {(loremipsum.cs) run} ifelse
 /latin1font where {pop} {(latin1font.cs) run} ifelse
 /paragraphs where {pop} {(paragraphs.cs) run} ifelse
@@ -8,7 +9,12 @@
 /red {255 0 0} def
 /green {0 255 0} def
 /blue {0 0 255} def
-/hr {  % x y color -  % horizontal rule, for debugging vertical space problems
+/cyan {0 255 255} def
+/yellow {255 255 0} def
+/magenta {255 0 255} def
+/black {0 0 0} bind def
+/hr ( x y color -  % horizontal rule
+  from HTML; for debugging vertical space problems) docstring {
   gsave setrgbcolor pagewidth 2 index dup add sub  % right margin = left
   3 1 roll #stack 0 #stack 3 1 roll #stack moveto #stack rlineto stroke #stack
   grestore
