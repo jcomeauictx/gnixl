89c06c2f6c295466672a8459185db6b40eb8e7e3
John Comeau <jc@unternet.net>
2024-11-29 07:24:44 -0800

    urllib.parse.quote not yet defined(?!)

--- noosepapers.com/urllib.cs
+++ noosepapers.com/urllib.cs
@@ -1,6 +1,6 @@
 #!/usr/local/casperscript/bin/ccs --
-/urllib 10 dict begin
-  /parse 10 dict begin
+/urllib 10 dict dup begin def
+  /parse 10 dict dup begin def
     /quote ( string#unquoted [string#safe string#unsafe?] - string#quoted
       like Python3's `urllib.parse.quote`
       but allows any range of safe and unsafe characters) docstring {
@@ -9,23 +9,23 @@
           [[latin1.a latin1.z 1 add] range {chr} forall] () string.join
           string.add def
         /DIGIT
-          ([latin1.0 latin1.9 1 add] range {chr} forall] () string.join def
+          [[latin1.0 latin1.9 1 add] range {chr} forall] () string.join def
         /unreserved ALPHA DIGIT string.add (-._~) string.add def
         /gen-delims (:/?#[]@) def
         /sub-delims (!$&'()*+,;=) def
         /reserved gen-delims sub-delims string.add def
-        
-      end  % local variables dict
       dup /unsafe {1 get} stopped {()} def
       /safe {0 get} stopped {()} def
       (safe: ) #only safe ##only (, unsafe: ) #only unsafe ##only
       (, unreserved: ) #only unreserved ##only
       (, reserved: ) #only reserved #
       (unquoted: ) #only #
+      end  % local variables dict
     } bind def
-  end def  % urllib.parse
-end def  % urllib
+  end  % urllib.parse
+end  % urllib
 scriptname (urllib) eq
-  {sys.argv 1 get sys.argv 2 subarray urllib.parse.quote}
+  %urllib ###
+  {sys.argv 1 get sys.argv [2] subarray #stack urllib.parse.quote}
 if
 % vim: tabstop=8 shiftwidth=2 expandtab softtabstop=2 syntax=postscr
