20202e859067a44e29d9fbeeb3c85892f7d723d1
John Comeau <jc@unternet.net>
2024-11-30 17:38:17 -0800

    about ready for loop

--- noosepapers.com/urllib.cs
+++ noosepapers.com/urllib.cs
@@ -14,13 +14,24 @@
         /gen-delims (:/?#[]@) def
         /sub-delims (!$&'()*+,;=) def
         /reserved gen-delims sub-delims string.add def
+        /escape ( string#character - string#escaped
+          escape the character, i.e. ( ) becomes (\04520)) docstring {
+          ord (\045) exch 8 3 string cvrs string.append
+        } bind def
       (stack before local defs: ) #only #stack
-      dup /unsafe exch #stack {1 get} stopped {pop pop () #stack} if def
-      /safe {0 get} stopped {pop ()} if def
+      dup /unsafe #stack exch {1 get} stopped {pop pop ()} if def
+      /safe exch #stack {0 get} stopped {#stack pop pop ()} if def
       (safe: ) #only safe ##only (, unsafe: ) #only unsafe ##only
       (, unreserved: ) #only unreserved ##only
       (, reserved: ) #only reserved #
-      (unquoted: ) #only ##
+      % convoluted logic we're using:
+      % IF marked as safe, don't escape it
+      % ELIF marked as unsafe, escape it
+      % ELIF in unreserved, don't escape it
+      % ELIF in reserved, escape it
+      % ELSE default is to leave it unescaped (*this may need to be changed*)
+      dup length 3 mul string exch % allow for entire string to be escaped
+      (remaining stack: ) #only #stack
       end  % local variables dict
     } bind def
   end  % urllib.parse
@@ -29,7 +40,7 @@ scriptname (urllib) eq
   %urllib ###
   {
     {sys.argv 1 get} stopped {pop pop (this is a test)} if
-    sys.argv [2] subarray #stack urllib.parse.quote
+    sys.argv [2] subarray urllib.parse.quote
   }
   if
 % vim: tabstop=8 shiftwidth=2 expandtab softtabstop=2 syntax=postscr
